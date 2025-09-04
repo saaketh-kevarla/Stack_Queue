f6f87cd1a5bc11d3c5a133746d90ef38cb7a5406
Saaketh Kevarla <skevarla@erpacloud.com>
2025-09-04 16:55:09 -0400

    Stack using LL

---
+++ src/Stack_Queues/Stack_LL.java
@@ -0,0 +1,60 @@
+package Stack_Queues;
+
+//stack using LinkedList
+class stack{
+    Node p;
+    Node head;
+    int currSize;
+
+    public void push(int a){
+        if(currSize == 0) {
+            p = new Node(a);
+            head = p;
+        }
+        else{
+            Node q = new Node(a);
+            q.next = head;
+            head = q;
+        }
+        currSize++;
+    }
+
+    public int pop(){
+        if(currSize == 0){
+            System.out.println("Stack is Empty");
+            System.exit(1);
+        }
+
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        System.out.println(temp.a +" is popped");
+        currSize--;
+
+        return temp.a;
+    }
+
+    public int top(){
+        return head.a;
+    }
+}
+
+class Node{
+    int a;
+    Node next;
+
+    public Node(int x){
+        this.a = x;
+    }
+}
+
+public class Stack_LL {
+    public static void main(String[] args) {
+        stack stack = new stack();
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+
+    }
+}
