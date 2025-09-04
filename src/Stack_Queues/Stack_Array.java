15a4aa05310099b80d5c4f835c5c983d6ccc878c
Saaketh Kevarla <skevarla@erpacloud.com>
2025-09-04 14:08:20 -0400

    Stack using array!

--- src/Stack_Queues/Stack_Array.java
+++ src/Stack_Queues/Stack_Array.java
@@ -1,11 +1,46 @@
 package Stack_Queues;
 
 class Stack_A{
+    int size;
+    int[] arr ;
+    int top = -1;
+
+    public Stack_A(int size){
+        this.size = size;
+        arr = new int[size];
+    }
+
+    int top(){
+        return arr[top];
+    }
+
+    int pop(){
+        int k = top;
+        top = top-1;
+        return arr[k];
+    }
+
+    void push(int x){
+        top++;
+        arr[top] = x;
+    }
+
+    int size(){
+        return top+1;
+    }
 
 }
 
 public class Stack_Array {
     public static void main(String[] args) {
+//        System.out.println("hello");
+        Stack_A stack1 = new Stack_A(1000);
+        stack1.push(5);
+        System.out.println(stack1.size());
+        stack1.push(6);
+        stack1.push(7);
+        System.out.println(stack1.pop());
+        System.out.println(stack1.size());
 
     }
 }
